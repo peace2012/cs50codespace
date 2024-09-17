aebf3f20eb2ec5510d2b4fb94e3aa0e969eef3c2
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-17 18:15:32 +0000

    Tue, Sep 17, 2024, 2:15 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -11,12 +11,22 @@ int main(void) {
     while (h < 1 || h > 8);
 
     for (int i = 0; i < h; i++) {
-        for (int j = 0; j < i; j++) {
-            printf("#  #");
+
+        for (int j = 0; j < 1; j++) {
+            printf(" ");
         }
+
+        for (int j = 0; j < 1; j++) {
+            printf("#");
+        }
+
         printf("#  ");
 
+for (int j = 0; j < 1; j++) {
+            printf("#");
+        }
 
         printf("#\n");
+
     }
 }
