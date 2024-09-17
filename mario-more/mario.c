82a1f91d13d33302cb3ffee78c7167e478cae03b
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-17 18:48:06 +0000

    Tue, Sep 17, 2024, 2:48 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,38 +1,48 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
 
-//initiates integer h or height of the pyramid//
+    // initiates integer h or height of the pyramid//
     int h;
 
-//do while loop verifying the int is between 1 and 8//
-    do {
+    // do while loop verifying the int is between 1 and 8//
+    do
+    {
         h = get_int("Height: ");
     }
     while (h < 1 || h > 8);
 
-/*loop repeats for height of the pyramid
-each loop will print one line of the pyramid*/
-    for (int i = 0; i < h; i++) {
+    /*loop repeats for height of the pyramid
+    each loop will print one line of the pyramid*/
+    for (int i = 0; i < h; i++)
+    {
 
-/*loop repeats the spaces before the first # sign
-
-        for (int j = h-i-1; j > 0; j--) {
+        /*loop repeats the spaces before the first # sign
+        a decreasing sequence*/
+        for (int j = h - i - 1; j > 0; j--)
+        {
             printf(" ");
         }
-
-        for (int k = 0; k < i; k++) {
+        /*loop prints the required number of # in the left side of pyramid
+        an increasing sequence from 0 to the height of pyramid less 1*/
+        for (int k = 0; k < i; k++)
+        {
             printf("#");
         }
 
+        // all pyramids include the character # followed by two spaces//
         printf("#  ");
 
-        for (int l = 0; l < i; l++) {
+        /*loop prints the required number of # to the right side of the pyramid
+        an increasing sequence as with the # on the left side*/
+        for (int l = 0; l < i; l++)
+        {
             printf("#");
         }
 
+        // all pyramids include the character # then a new line//
         printf("#\n");
-
     }
 }
