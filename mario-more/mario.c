87c3238ac59a01241fb6616731dd226dd50320bd
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-17 17:49:38 +0000

    Tue, Sep 17, 2024, 1:49 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,11 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int h;
 int main(void) {
 
     do {
-        h = get_int("Height: ");
+        int h = get_int("Height: ");
     }
     while (h < 1 || h > 8);
 }
