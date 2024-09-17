f233026065ef99f76d5a49c8e96a0df6c6aeb018
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-17 18:41:28 +0000

    Tue, Sep 17, 2024, 2:41 PM -04:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,14 +3,20 @@
 
 int main(void) {
 
+//initiates integer h or height of the pyramid//
     int h;
 
+//do while loop verifying the int is between 1 and 8//
     do {
         h = get_int("Height: ");
     }
     while (h < 1 || h > 8);
 
+/*loop repeats for height of the pyramid
+each loop will print one line of the pyramid*/
     for (int i = 0; i < h; i++) {
+
+/*loop repeats the spaces before the first # sign
 
         for (int j = h-i-1; j > 0; j--) {
             printf(" ");
@@ -22,7 +28,7 @@ int main(void) {
 
         printf("#  ");
 
-for (int l = 0; l < i; l++) {
+        for (int l = 0; l < i; l++) {
             printf("#");
         }
 
