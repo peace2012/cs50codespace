4eed758911d2e746a75e1f605bb740e3307b2aed
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-18 15:10:54 +0000

    Wed, Sep 18, 2024, 11:10 AM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,5 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
+
+int count_number_length(long number)
+int first_two_digits (long number, int card_number_length)
 
 int main(void)
 {
@@ -27,29 +31,26 @@ while (card_number !== )
 }
 
 // function prompts user for card number //
-void get_card_number(number)
+void get_card_number(long number)
 {
 number = get_long("Number: ");
-}
-
-// isolate first two digits of the card number //
-int first_two_digits (long number, int card_number_length)
-{
-    return int first_two digits = number / ((card_number_length-2)*10);
 }
 
 // count the number of digits in the cart number //
 int count_number_length(long number)
 {
-    // initialize counter to 0 //
     int card_number_length = 0;
-
-    // loop to divide number by 10 until 0 //
     while (number > 0)
     {
         number = number / 10;
-        // increment the county in each iteraction //
         card_number_length++;
     }
 return card_number_length
+}
+
+// isolate first two digits of the card number //
+int first_two_digits (long number, int card_number_length)
+{
+    int first_two_digits;
+    return first_two_digits = number / pow(10, card_number_length-2);
 }
