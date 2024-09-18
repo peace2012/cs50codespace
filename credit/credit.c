eb5ad4098628717571cb816e849b07c3d9832291
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-18 18:17:12 +0000

    Wed, Sep 18, 2024, 2:17 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -19,9 +19,14 @@ int main(void)
 
     int card_number_length = count_number_length(card_number);
 
-    char* first_array = create_array(0, card_number, card_number_length);
+    char *first_array = create_array(0, card_number, card_number_length);
+    if (first_array !=NULL)
+    {
+        printf("%s\n", first_array);
+        free(first_array);
+    }
 
-    printf("%s\n", first_array);
+
 
     // check card type using length and first two digits //
 
