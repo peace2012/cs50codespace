b21b12a66081d76f200073970ff0afa336ce26a7
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-18 23:27:12 +0000

    Wed, Sep 18, 2024, 7:27 PM -04:00

--- credit/credit.c
+++ credit/credit.c
@@ -180,33 +180,39 @@ int type_of_card(long card_number)
 {
     int card_length = count_number_length(card_number);
     int two_digits = first_two_digits(card_number, card_length);
+    char response;
 
     if (card_length == 15 && two_digits == 37)
     {
-        return 1;
+        response = "AMEX\n";
+        return response;
     }
     else if (card_length == 16)
     {
-        
+
         switch (two_digits)
         {
             case 22:
             case 55:
             case 51:
             case 52:
-                return "MASTERCARD";
+                response = "MASTERCARD\n";
+                return response;
                 break;
             case 41:
             case 40:
             case 49:
-                return 3;
+                response = "VISA\n";
+                return response;
                 break;
             default:
-                return 4;
+                response = "INVALID\n";
+                return response;
         }
     }
     else
-        return 4;
+        response = "INVALID\n";
+        return response;
 }
 
 // // Define arrays
