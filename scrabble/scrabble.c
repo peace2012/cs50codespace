81b2408ebc769cfc6b461e43a4f209cee640335a
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-26 19:35:57 +0000

    Thu, Sep 26, 2024, 3:35 PM -04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -10,32 +10,55 @@ typedef struct {
 
 WordNumber scrabble[] = {{"aeilnorstu", 1}, {"dg", 2}, {"bcmp", 3}, {"fhvwy", 4}, {"k", 5}, {"jx", 8}, {"qz", 10}};
 
-string get_word (string player);
-bool is_word(const char *str);
+int player_score (string input_name);
+string get_input (string input_name);
+bool is_string(const char *str);
 string to_lowercase (char *str);
 int get_score (const char *str, WordNumber array[], int length);
 
 int main (void)
 {
-    string player_1_word = get_word("Player 1");
+    int p1_score = player_score("Player 1");
+    int p2_score = player_score("Player 2");
 
-    player_1_word = to_lowercase(player_1_word);
+    // string player_1_word = get_input("Player 1");
 
-    int length = sizeof(scrabble) / sizeof(scrabble[0]);
+    // player_1_word = to_lowercase(player_1_word);
 
-    int player_1_score = get_score(player_1_word, scrabble, length);
-    
-    printf("%i\n", player_1_score);
+    // int length = sizeof(scrabble) / sizeof(scrabble[0]);
+
+    // int player_1_score = get_score(player_1_word, scrabble, length);
+
+// logic to compare scores
+if (p1_score < p2_score)
+{
+    printf("Player 2 wins!\n");
+}
+if (p1_score == p2_score)
+{
+    printf("Tie!\n");
+}
+else
+{
+    printf("Player 1 wins!\n");
 }
 
-// function uses variable number of players
-// runs a loop to request input word, outputs string
+}
+
+    int player_score (string input_name)
+    {
+        string word = get_input(input_name);
+        word = to_lowercase(word);
+        int length = sizeof(scrabble) / sizeof(scrabble[0]);
+        int score = get_score(word, scrabble, length);
+        return score;
+    }
 
 // gets string from player
-string get_word (string player)
+string get_input (string input_name)
 {
-    string word = get_string("%s: ", player);
-    if (is_word(word) == false)
+    string word = get_string("%s: ", input_name);
+    if (is_string(word) == false)
     {
         fprintf(stderr, "Error enter valid word.\n");
         exit(1);
@@ -44,7 +67,7 @@ string get_word (string player)
 }
 
 // checks user input is valid word
-bool is_word(const char *str)
+bool is_string(const char *str)
 {
     for (int i = 0; str[i] != '\0'; i++)
     {
@@ -66,10 +89,7 @@ string to_lowercase (char *str)
     return str;
 }
 
-// string is an array
-// compare each letter to an array of scrabble letters and scores
-// output the integer found
-
+// loop through struct find input char in WordNumber array, get letter score, return total score
 int get_score (const char *str, WordNumber array[], int length)
 {
     int score = 0;
@@ -87,4 +107,3 @@ int get_score (const char *str, WordNumber array[], int length)
     }
     return score;
 }
-
