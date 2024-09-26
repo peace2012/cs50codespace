0e1977f05b19f1baa3e1b50c5c878523c98912a0
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-26 17:37:21 +0000

    Thu, Sep 26, 2024, 1:37 PM -04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 #include <cs50.h>
 
-string get_word (int number);
+string get_word (string player);
 
-// variable for number of players
-const int N = 2;
+// // variable for number of players
+// const int N = 2;
 
 int main (void)
 {
-    for (int i = 0; i < N; i++)
-    {
-        const get_word(i+=);
-    }
+    string player_1_word = get_word("Player 1");
+    string player_2_word = get_word("Player 2");
+
+
+    // int player_1_score = 0;
+    // int player_2_score = 0;
+
 }
 
 // function uses variable number of players
 // runs a loop to request input word, outputs string
 
-string get_word (int number)
+string get_word (string player)
 {
-        string word = get_string("Player %i:", number++);
-        return word;
+        string word = get_string("%s: ", player);
+        if (word == string)
+        {
+            return word;
+        }
+        else
+        {
+            printf(stderr, "Please enter a word.");
+            exit(1);
+        }
+
 }
 
 // string is an array
 // compare each letter to an array of scrabble letters and scores
 // output the integer found
 
-int compare (char letter)
-{
+// int compare (char letter)
+// {
 
-}
+// }
