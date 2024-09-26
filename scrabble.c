1bf2480b69992b5094dd2f406343c07ed0a72a80
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-26 17:20:21 +0000

    Thu, Sep 26, 2024, 1:20 PM -04:00

---
+++ scrabble.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// variable for number of players
+const int N = 2;
+
+int main (void)
+{
+
+}
+
+// function uses variable number of players
+// runs a loop to request input word, outputs string
+
+string get_input (int number)
+{
+    for (i = 0; i < number; i++)
+    {
+        get_string()
+    }
+}
+
+// string is an array
+// compare each letter to an array of scrabble letters and scores
+// output the integer found
+
+int compare (char letter)
+{
+
+}
