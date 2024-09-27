b2a26c4023ad0454ea91dda8fe64a4c37c1e3216
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-27 14:36:02 +0000

    Fri, Sep 27, 2024, 10:36 AM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -28,7 +28,7 @@ int main (void)
         float L = ( (float)count_alpha(text) / ++no_of_spaces );
         printf("%f\n", L);
 
-    int result = 0.0588 * (L*100) - 0.296 * (S*100) - 15.8;
+    int result = (0.0588 * (L*100)) - 0.296 * (S*100) - 15.8;
     printf("%i\n", result);
 
     }
