7e135a8b61607554a7ad8215dd23e3c8fd1c30b6
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-27 15:08:52 +0000

    Fri, Sep 27, 2024, 11:08 AM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,22 +10,28 @@ int count_alpha(const char *str);
 
 int main(void)
 {
+    // get text string
     string text = get_input("Text");
 
+    // count number of spaces in text string
     int no_of_spaces = count(text, ' ');
 
+    // convert spaces to number of words
     float no_of_words = ++no_of_spaces;
 
-    // total letters divided by total words
+    // total letters divided by total words *100
     float L = (count_alpha(text) / no_of_words) * 100;
 
     // count punctuation to find number of sentences
     int total_sentences = count(text, '!') + count(text, '?') + count(text, '.');
 
+    // total sentences divided by total words *100
     float S = (total_sentences / no_of_words) * 100;
 
+    // use formula to calculate result, round result to nearest integer
     int result = round(0.0588 * L - 0.296 * S - 15.8);
 
+    // logic to determine printed output
     if (result <= 1)
     {
         printf("Before Grade 1\n");
@@ -44,7 +50,6 @@ int main(void)
 string get_input(string str)
 {
     string output = get_string("%s: ", str);
-
     return output;
 }
 
@@ -60,7 +65,6 @@ int count(const char *str, const int alpha)
             count++;
         }
     }
-
     return count;
 }
 
