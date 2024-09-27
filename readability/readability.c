58dbae896593be94efd351f72e14c3e8a3a9ae19
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-27 15:01:37 +0000

    Fri, Sep 27, 2024, 11:01 AM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,29 +1,30 @@
 #include <cs50.h>
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
 
-string get_input (string str);
-int count (const char *str, const int alpha);
+string get_input(string str);
+int count(const char *str, const int alpha);
 int count_alpha(const char *str);
 
-int main (void)
+int main(void)
 {
     string text = get_input("Text");
 
     int no_of_spaces = count(text, 32);
-    if (no_of_spaces <= 100) {
+    if (no_of_spaces <= 100)
+    {
 
         float no_of_words = ++no_of_spaces;
 
         // total letters divided by total words
-        float L = ( count_alpha(text) / no_of_words ) * 100;
+        float L = (count_alpha(text) / no_of_words) * 100;
 
         // count punctuation to find number of sentences
         int total_sentences = count(text, 33) + count(text, 63) + count(text, 46);
 
-        float S = ( total_sentences / no_of_words ) * 100;
+        float S = (total_sentences / no_of_words) * 100;
 
         int result = round(0.0588 * L - 0.296 * S - 15.8);
 
@@ -31,9 +32,13 @@ int main (void)
         {
             printf("Before Grade 1\n");
         }
+        else if (result >= 16)
+        {
+            printf("Grade 16+\n");
+        }
         else
         {
-        printf("Grade %i\n", result);
+            printf("Grade %i\n", result);
         }
     }
     else
@@ -43,7 +48,7 @@ int main (void)
 }
 
 // gets text input
-string get_input (string str)
+string get_input(string str)
 {
     string output = get_string("%s: ", str);
 
@@ -51,13 +56,14 @@ string get_input (string str)
 }
 
 // count the number of something in a string
-int count (const char *str, const int alpha)
+int count(const char *str, const int alpha)
 {
     int count = 0;
 
-        for (int i = 0; str[i] !='\0'; i++)
+    for (int i = 0; str[i] != '\0'; i++)
     {
-        if (str[i] == alpha) {
+        if (str[i] == alpha)
+        {
             count++;
         }
     }
