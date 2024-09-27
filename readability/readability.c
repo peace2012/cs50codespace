d20c1559592ad51824908cd971208d268d091f33
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-27 13:13:04 +0000

    Fri, Sep 27, 2024, 9:13 AM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,1 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
 
+string get_input(string str);
+
+// index = 0.0588 * L - 0.296 * S - 15.8
+// L = average number of letters per 100 words
+// S = average number of sentences per 100 words
+// Would you like them here or there? I would not like them here or there. I would not like them anywhere.
+
+int main (void)
+{
+    string text = get_input("Text");
+    printf("%s\n", text);
+
+    // int index = 0.0588 * L - 0.296 * S - 15.8;
+    // printf("%i\n", index);
+}
+
+// gets text input
+string get_input(string str)
+{
+    string output = get_string("%s: ", str);
+
+    return output;
+}
