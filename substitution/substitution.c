0833fad62717f1ed345c33c96dad925c7e5f51e8
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-30 14:14:16 +0000

    Mon, Sep 30, 2024, 10:14 AM -04:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int main (int argc, string argv[])
 {
 // if > 1 command line argument return error
-if (argc >= 2)
-{
-    printf("Enter 1 key of 26 characters");
-    return 1;
-}
-else
-{
-// loop through array if key has 26 characters all alpha
-if
+    if (argc >= 2)
+    {
+        printf("Error - enter 1 key of 26 characters");
+        return 1;
+    }
+
+    else
+    {
+        string key = argv[1];
+
+        // get length of key entered
+        int key_length = strlen(key);
+
+        // if length of key is not 26 return error
+        if (key_length != 26)
+        {
+            printf("Error - key must be 26 characters");
+            return 1;
+        }
+
+        else
+        {
+            // loop through array confirm key has 26 characters all alpha
+            for (int i = 0; i <= key_length ; i++)
+            {
+                isalpha(key[i]);
+            }
+        }
+
+
 
 
 // get string
@@ -19,3 +42,6 @@ if
 
 }
 }
+
+// check if char in string is alphabetical
+
