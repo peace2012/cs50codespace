8474712326a0a0ec3b3f7476296a4bd2d003d06a
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-09-30 14:02:51 +0000

    Mon, Sep 30, 2024, 10:02 AM -04:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,13 +2,20 @@
 
 int main (int argc, string argv[])
 {
-// get user input
+// if > 1 command line argument return error
+if (argc >= 2)
+{
+    printf("Enter 1 key of 26 characters");
+    return 1;
+}
+else
+{
+// loop through array if key has 26 characters all alpha
+if
 
 
-// check to see if user input is valid
-// key must contain 26 characters
+// get string
 
 
-
-
+}
 }
