0bf7e2d4e68ed4fc16a44fe02144c7f6c445f0ce
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-10-04 18:07:06 +0000

    Fri, Oct 4, 2024, 2:07 PM -04:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -89,7 +89,13 @@ int main(int argc, string argv[])
         printf("\n");
     }
 
-    
+        for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            printf("[%i][%i] %i\n", i, j, preferences[i][j]);
+        }
+    }
 
     add_pairs();
     sort_pairs();
@@ -158,11 +164,11 @@ void add_pairs(void)
         }
     }
 
-    for (int i = 0; i < pair_count; i++)
-    {
-        printf("%i, ", pairs[i].winner);
-        printf("%i\n", pairs[i].loser);
-    }
+    // for (int i = 0; i < pair_count; i++)
+    // {
+    //     printf("%i, ", pairs[i].winner);
+    //     printf("%i\n", pairs[i].loser);
+    // }
     return;
 }
 
