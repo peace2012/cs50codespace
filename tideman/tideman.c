ff836b9b06ee22229191e1099459d1270905a040
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-10-04 17:43:45 +0000

    Fri, Oct 4, 2024, 1:43 PM -04:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -129,7 +129,7 @@ void record_preferences(int ranks[])
     {
         for (int j = ranks[i]; j < candidate_count; j++)
         {
-            ++preferences[i][--j];
+            ++preferences[i][j];
         }
     }
     return;
