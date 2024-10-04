32c9797dcfbbceafcfd65ad191e4b6d4790761fb
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-10-04 16:47:30 +0000

    Fri, Oct 4, 2024, 12:47 PM -04:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -134,14 +134,12 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            if (strcmp(ranks[i], candidates[j]) == 0)
+        if (strcmp(ranks[i], candidates[j]) == 0)
             {
-                for (int k = 0; k < candidate_count; k++)
+                for (int j = 0; k < candidate_count; j++)
                 {
-                    if (j != k) {
-                        ++preferences[j][k];
+                    if (i != j) {
+                        ++preferences[i][j];
                     }
                 }
             }
