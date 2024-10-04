a7d5436a60903351958386230cf117e767e9cb6c
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-10-04 17:56:00 +0000

    Fri, Oct 4, 2024, 1:56 PM -04:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -144,7 +144,6 @@ void add_pairs(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            
             if (preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
