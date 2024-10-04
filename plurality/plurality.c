d2ca4f7d6fe02725c2278a4a30dd9189b6078bbb
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-10-04 14:34:13 +0000

    Fri, Oct 4, 2024, 10:34 AM -04:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,7 +71,7 @@ bool vote(string name)
 
     for ( int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].name == name)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             ++candidates[i].votes;
             return true;
