85611344cb1195f1c27303fc38f8c8f93f38756d
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-10-04 16:43:00 +0000

    Fri, Oct 4, 2024, 12:43 PM -04:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -109,7 +109,7 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp(candidates[i], name) == 0)
         {
-            ranks[rank] = name;
+            ranks[i] = rank;
             return true;
         }
     }
