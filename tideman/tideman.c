207de8b5a9c70c94fd40ebcf024508f6598c28a0
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-10-07 15:54:16 +0000

    Mon, Oct 7, 2024, 11:54 AM -04:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -285,14 +285,14 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0); j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             if (locked[j][i] == true)
             {
-
+                break;
             }
         }
-        winner = candidates[]
+        winner = candidates[i];
     }
     printf("%s\n", winner);
     return;
