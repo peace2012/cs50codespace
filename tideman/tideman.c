da9bf0733253ba2369443998cc56612f58811b49
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-10-07 18:08:46 +0000

    Mon, Oct 7, 2024, 2:08 PM -04:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -223,6 +223,11 @@ bool creates_cycle(int winner, int loser)
         return true;
     }
 
+    if (locked[loser][winner] == true)
+    {
+        return true;
+    }
+
     for (int i = 0; i < candidate_count; i++)
     {
         if (locked[loser][i] == true)
