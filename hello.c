73f16ca27e82a349a818472823bbf1b5bbf0abe0
peace2012 <peaceerio12@gmail.com>
2025-04-21 16:04:59 +0000

    Mon, Apr 21, 2025, 4:04 PM +00:00

--- hello.c
+++ hello.c
@@ -6,7 +6,6 @@ int main(void)
     printf("hello\n\n");
     string answer=get_string("what is your name? ");
     printf("hello, %s\n",answer);
-    string answer=get_string("how are you, %s\n", printf("hello, %s\n", answer) );
 }
 
 
